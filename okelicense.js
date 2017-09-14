bd233a000173adbece6e2cb800ca07f78e6613c4
andiyudrianto <31957602+andiyudrianto@users.noreply.github.com>
2017-09-15 02:21:17 +0700

    Update okelicense.js

--- okelicense.js
+++ okelicense.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
     $(".deskripsi").html(deskripsi);
     var harga = $(".harga").html();
     $(".price").html(harga);
-    $(".hGfzU").html("Support By : <a href='http://www.oketemplate.com'>OKEtemplate</a>");
+    $(".hGfzU").html("Support By : <a href='https://www.lapaksuplemen.com'>Lapaksuplemen</a>");
     $('a[href=#top]').click(function() {
         $('html, body').animate({
             scrollTop: 0
